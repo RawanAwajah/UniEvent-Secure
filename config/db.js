37007db5c813887a318492be3ff3f4e6c55a6e79
Rawan Awajah <rawanawajah@gmail.com>
2026-06-18 16:51:10 +0300

    Move database credentials to environment variables

--- config/db.js
+++ config/db.js
@@ -1,18 +1,6 @@
-const mysql = require("mysql2");
-
-const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Root123!",
-    database: "unievent_secure"
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
-
-connection.connect((err) => {
-    if (err) {
-        console.error("Connection failed:", err);
-    } else {
-        console.log("Connected to MySQL");
-    }
-});
-
-module.exports = connection;
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 function isAdmin(req, res, next) {
 
     if (!req.session.userId) {
